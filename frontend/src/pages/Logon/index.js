8b13886916a954efd04b21f34728b1a28e8e136e
EduardoBrasil <brasil.eduardo1@gmail.com>
2020-04-02 12:04:00 -0300

    Tela de cadastro e implementação das rotas

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './styles.css';
 
-import logoImg from '../../assets/logo.svg'
 import { FiLogIn } from 'react-icons/fi';
-import heroesImg from '../../assets/heroes.png'
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
     return (
@@ -18,10 +20,10 @@ export default function Logon() {
                     <input placeholder="Sua ID"/>
                     <button className ="button" type="submit">Entrar</button>
 
-                    <a href ="/register">
+                    <Link className="back-link" to ="/register">
                         <FiLogIn size={16} color="#E02041"/>
                          Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
